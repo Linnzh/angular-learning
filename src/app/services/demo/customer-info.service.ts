057184ad7f9c62cb24a4d6d5c853fa72b194cd1c
Linnzh <reg.lynnzh@gmail.com>
2021-04-18 17:39:48 +0800

    use dependency injection

--- src/app/services/demo/customer-info.service.ts
+++ src/app/services/demo/customer-info.service.ts
@@ -5,5 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class CustomerInfoService {
 
+  info1: string[] = ['Linn Zhou', '1996-09-08', 'reg.lynnzh@gmail.com', 'Hangzhou'];
+  info2: string[] = ['Bob Brown', '1997-09-08', 'reg.bob@gmail.com', 'Nanchang'];
+  info3: string[] = ['Alice Taylor', '1998-09-08', 'reg.alice@gmail.com', 'Nanjing'];
+
+  getInfo1(): string[] {
+    return this.info1;
+  }
+  getInfo2(): string[] {
+    return this.info2;
+  }
+  getInfo3(): string[] {
+    return this.info3;
+  }
+  
   constructor() { }
+
 }
