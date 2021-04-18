1eb0172838e2958dc7021d7dd559a591f6df6ee6
Linnzh <reg.lynnzh@gmail.com>
2021-04-18 17:28:54 +0800

    generate a component

---
+++ src/app/components/demo/service-and-dependency-injection/service-and-dependency-injection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-and-dependency-injection',
+  templateUrl: './service-and-dependency-injection.component.html',
+  styleUrls: ['./service-and-dependency-injection.component.scss']
+})
+export class ServiceAndDependencyInjectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
