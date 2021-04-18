91335413d1748ffcb9b53f839d2d5c2729f76af3
Linnzh <reg.lynnzh@gmail.com>
2021-04-18 17:50:04 +0800

    generate a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GenerateAComponentComponent } from './components/demo/generate-a-component/generate-a-component.component';
 import { CustomizeComponentNameComponent } from './components/demo/customize-component-name/customize-component-name.component';
 import { ServiceAndDependencyInjectionComponent } from './components/demo/service-and-dependency-injection/service-and-dependency-injection.component';
+import { AboutFormElementComponent } from './components/demo/about-form-element/about-form-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GenerateAComponentComponent,
     CustomizeComponentNameComponent,
-    ServiceAndDependencyInjectionComponent
+    ServiceAndDependencyInjectionComponent,
+    AboutFormElementComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/demo/about-form-element/about-form-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-form-element',
+  templateUrl: './about-form-element.component.html',
+  styleUrls: ['./about-form-element.component.scss']
+})
+export class AboutFormElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
